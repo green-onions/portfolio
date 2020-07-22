899d66df349e8be001b9e6e6c6549b6d8889a5de
Green <regnault.marien@gmail.com>
2020-07-22 13:50:34 +0200

    Fixtures added

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('green-onions');
+        $user->setFirstname('Marien');
+        $user->setLastname('Regnault');
+        $user->setJobTitle('Développeur web full-stack');
+        $user->setDescription('Après plusieurs années à travailler comme journaliste, 
+        je me suis reconverti dans le développement web. Curieux et rigoureux, passionné du numérique, 
+        j\'ai suivi une formation en PHP - Symfony et suis à la recherche d\'opportunités pour mettre 
+        à l\'épreuve cette nouvelle qualification. Vous retrouverez sur ce site toutes les réalisations 
+        auxquelles j\'ai participé.');
+        $user->setPassword('coucou');
+        $user->setRoles(['ROLE_ADMIN']);
+        $manager->persist($user);
+        $this->addReference('user', $user);
+        $manager->flush();
+    }
+}
